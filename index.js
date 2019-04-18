e7b913e8de9d0ee44f54749acab811519e4e97d0
Sowemimo Bamidele <bigbams@users.noreply.github.com>
2019-04-18 21:17:14 +0100

    initial

---
+++ index.js
@@ -0,0 +1,19 @@
+import 'bootstrap'
+import './sass/main.scss'
+// import $ from "jquery";
+// import 'owl.carousel/dist/assets/owl.carousel.css';
+// import 'owl.carousel';
+// $(document).ready(function(){
+//     $('.owl-carousel').owlCarousel({
+//         items:4,
+//         loop:false,
+//         center:true,
+//         margin:10,
+//         URLhashListener:true,
+//         autoplayHoverPause:true,
+//         startPosition: 'URLHash'
+//     });
+// });
+// $(document).ready(function(){
+//   $('.owl-carousel').owlCarousel();
+// });
